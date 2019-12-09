6efbed7a69937bda5be20ac6b00c8f73dc59179f
Kevin Bae <kooddog15@gmail.com>
2019-12-09 12:51:52 -0500

    docstrings

--- map.py
+++ map.py
@@ -20,6 +20,7 @@ class Map():
         self.game_over = False
 
     def __repr__(self):
+        """Draw map interface for user"""
         rep = ''
         repfn = lambda o: ' ' if o == None else repr(o)
         for row in self.arr:
@@ -41,9 +42,12 @@ class Map():
         return rep
 
     def __getitem__(self, pos):
+        """Returns item at specific position"""
         return self.at(pos)
 
     def __contains__(self, item: Item):
+        """Returns an item id of the passed in item
+           If it is not an item, it returns False"""
         if item.type == 'member':
             return item.id in self.members
         elif item.type == 'resource':
@@ -52,12 +56,14 @@ class Map():
             return False
 
     def add_species_member(self, species_id):
+        """Update species count"""
         if species_id in self._species_counts:
             self._species_counts[species_id] += 1
         else:
             self._species_counts[species_id] = 1
 
     def remove_species_member(self, species_id):
+        """Update species count"""
         if species_id in self._species_counts:
             self._species_counts[species_id] -= 1
             if self._species_counts[species_id] == 0:
@@ -65,15 +71,18 @@ class Map():
 
     @property
     def species_counts(self):
+        """Return species count"""
         return self._species_counts.copy()
 
     def add(self, item, pos):
+        """Place item in new position"""
         self.remove(self.at(pos))
         self.empty_pos.discard(pos)
         self._setat(pos, item)
         self._setloc(item, pos)
 
     def remove(self, item):
+        """Remove item from current position"""
         if item == None: return
         item_pos = self.loc(item)
         self._setat(item_pos, None)
@@ -82,6 +91,7 @@ class Map():
 
 
     def move(self, item, pos):
+        """Update item position"""
         self.remove(self.at(pos))
         item_pos = self.loc(item)
         self._setat(item_pos, None)
@@ -91,6 +101,7 @@ class Map():
         self.empty_pos.discard(pos)
 
     def _setloc(self, item, pos):
+        """Set location in our datastructures"""
         locations = None
         if item.type == 'member':
             locations = self.members
@@ -102,6 +113,7 @@ class Map():
             del locations[item.id]
 
     def loc(self, item: Item):
+        """Rturns our location representation for an item"""
         locations = None
         if item.type == 'member':
             locations = self.members
@@ -110,21 +122,26 @@ class Map():
         return locations[item.id]
 
     def _setat(self, pos, item: Item):
+        """Set item to 2d array"""
         self.arr[pos[0]][pos[1]] = item
 
     def at(self, pos):
+        """Return item in our 2d array representation"""
         return self.arr[pos[0]][pos[1]]
 
     def in_bounds(self, pos):
+        """Return boolean if position representation is in bound"""
         if pos[0] < 0 or pos[0] > self.height - 1: return False
         if pos[1] < 0 or pos[1] > self.width - 1: return False
         return True
 
     @property
     def is_game_over(self):
+        """return boolean if game is over or not"""
         return self.game_over
 
     def check_game_over(self):
+        """Set game_over to True if only 1 or less species left"""
         if len(self.species_counts) <= 1:
             self.game_over = True
 
